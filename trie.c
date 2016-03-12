3297020ce0944513692dc061f0e2202cefb946d3
sanjana96 <sanjana0796@gmail.com>
2016-03-12 23:40:44 +0530

    Update trie.c

--- trie.c
+++ trie.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#define SIZE 26
+#define LASCI 96	//lowerASCII
 typedef struct node{
 	char a;
-	struct node* r[26];
+	struct node* r[SIZE];
 }node;
 void insert(char* a,node* root);
 void preorder(node* root);
@@ -17,18 +19,19 @@ void insert(char* a,node* root){
 	int i = 0;
 	int len = strlen(a);
 	if(len>0){
-		if(!(root->r[a[0]-97])){
-			root->r[(a[0]-97)] = malloc(sizeof(node));
-			root->r[(a[0]-97)] -> a = a[0];
+		int t = a[0] - LASCI;
+		if(!(root->r[t])){
+			root->r[t] = malloc(sizeof(node));
+			root->r[t] -> a = a[0];
 		}
-		insert(a+1,root->r[(a[0]-97)]);				
+		insert(a+1,root->r[t]);				
 	}		
 }
 void preorder(node* root){
 	if(root){
 		printf("%c",root -> a);
 		int i = 0;
-		for(;i<26;i++){
+		for(;i<SIZE;i++){
 			preorder(root -> r[i]);
 		}			
 	}
